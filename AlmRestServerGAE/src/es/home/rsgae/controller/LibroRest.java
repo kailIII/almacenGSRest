db16b51bac63e4dbbc2ef4a388f953a4ea7ff67d
deesebc <sinliston@gmail.com>
2013-04-22 15:03:20 +0200

    - eliminamos configuracion spring no usada - incluimos mapeo a traves de dozer - creamos relacion dozer entre Libro y LibroJson

--- AlmRestServerGAE/src/es/home/rsgae/controller/LibroRest.java
+++ AlmRestServerGAE/src/es/home/rsgae/controller/LibroRest.java
@@ -7,6 +7,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 
 import org.apache.log4j.Logger;
+import org.dozer.Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,16 +28,8 @@ public class LibroRest {
 
 	private final Logger logger = Logger.getLogger(getClass());
 
-	// @Autowired
-	// protected Mapper mapper;
-
-	// private static final Map<Long, LibroJson> books = new HashMap<Long,
-	// LibroJson>();
-	// static {
-	// books.put(1l, new LibroJson(1l, "book1", "author1", "argument1", "100"));
-	// books.put(2l, new LibroJson(2l, "book2", "author2", "argument2", "200"));
-	// books.put(3l, new LibroJson(3l, "book3", "author3", "argument3", "300"));
-	// }
+	@Autowired
+	protected Mapper mapper;
 
 	@RequestMapping(method = RequestMethod.POST, produces = "application/json")
 	public @ResponseBody
@@ -43,12 +37,11 @@ public class LibroRest {
 		logger.info("LibroRest - create - init");
 		EntityManager em = EMF.get().createEntityManager();
 		EntityTransaction transaction = em.getTransaction();
-		logger.info("Transaction actived ? - "+transaction.isActive());
+		logger.info("Transaction actived ? - " + transaction.isActive());
 		transaction.begin();
-		logger.info("Transaction actived ? - "+transaction.isActive());
+		logger.info("Transaction actived ? - " + transaction.isActive());
 		try {
-			// Libro bean = mapper.map(form, Libro.class);
-			Libro bean = getLibro(form);
+			Libro bean = mapper.map(form, Libro.class);
 			em.persist(bean);
 			transaction.commit();
 		} catch (Exception except) {
@@ -62,25 +55,23 @@ public class LibroRest {
 
 	@SuppressWarnings("unchecked")
 	@RequestMapping(method = RequestMethod.GET, produces = "application/json")
-	public @ResponseBody List<LibroJson> findAll() {
+	public @ResponseBody
+	List<LibroJson> findAll() {
 		logger.info("LibroRest - findAll - init");
 		EntityManager em = EMF.get().createEntityManager();
 		List<LibroJson> exit = new ArrayList<LibroJson>();
 		try {
-			String query = "select l from "+Libro.class.getName()+" l";
-			List<Libro> beans =  em.createQuery(query).getResultList();
-			// json = mapper.map(bean, LibroJson.class);
-//			JPAQuery
-//			List<Libro> calls = (List<Libro>) em.newQuery(query).execute();
+			String query = "select l from " + Libro.class.getName() + " l";
+			List<Libro> beans = em.createQuery(query).getResultList();
 
 			if (beans != null) {
 				LibroJson json = null;
 				for (Libro bean : beans) {
-					json = getJson(bean);
+					json = mapper.map(bean, LibroJson.class);
 					exit.add(json);
 				}
-				logger.info("Listado size: "+exit.size());
-			}else{
+				logger.info("Listado size: " + exit.size());
+			} else {
 				logger.info("Listado nulo");
 			}
 		} catch (Exception except) {
@@ -88,10 +79,10 @@ public class LibroRest {
 		} finally {
 			em.close();
 		}
-		logger.info("LibroRest - create - end");
+		logger.info("LibroRest - findAll - end");
 		return exit;
 	}
-	
+
 	@RequestMapping(method = RequestMethod.GET, value = "/{id}", produces = "application/json")
 	public @ResponseBody
 	LibroJson findById(@PathVariable final String id) {
@@ -100,8 +91,7 @@ public class LibroRest {
 		LibroJson json = null;
 		try {
 			Libro bean = em.find(Libro.class, id);
-			// json = mapper.map(bean, LibroJson.class);
-			json = getJson(bean);
+			json = mapper.map(bean, LibroJson.class);
 		} catch (Exception except) {
 			logger.error("Error al obtener el libro: ", except);
 		} finally {
@@ -110,43 +100,23 @@ public class LibroRest {
 		return json;
 	}
 
-	private LibroJson getJson(final Libro bean) {
-		LibroJson json = new LibroJson();
-		json.setId(bean.getIdent());
-		json.setArgumento(bean.getArgumento());
-		json.setAutor(bean.getAutor());
-		json.setNombre(bean.getNombre());
-		json.setPaginas(bean.getPaginas());
-		return json;
-	}
-
-	private Libro getLibro(final LibroJson json) {
-		Libro bean = new Libro();
-		bean.setIdent(json.getId());
-		bean.setArgumento(json.getArgumento());
-		bean.setAutor(json.getAutor());
-		bean.setNombre(json.getNombre());
-		bean.setPaginas(json.getPaginas());
-		return bean;
-	}
-
 	@ResponseStatus(value = HttpStatus.NO_CONTENT)
 	@RequestMapping(method = RequestMethod.PUT, value = "/{id}")
-	public void merge(@PathVariable(value = "id") final Long id, @RequestBody final LibroJson form) {
+	public void merge(@PathVariable(value = "id") final Long id,
+			@RequestBody final LibroJson form) {
 		logger.info("LibroRest - merge - init");
 		EntityManager em = EMF.get().createEntityManager();
 		EntityTransaction transaction = em.getTransaction();
-		logger.info("Transaction actived ? - "+transaction.isActive());
+		logger.info("Transaction actived ? - " + transaction.isActive());
 		transaction.begin();
-		logger.info("Transaction actived ? - "+transaction.isActive());
+		logger.info("Transaction actived ? - " + transaction.isActive());
 		try {
-			// Libro bean = mapper.map(form, Libro.class);
-			Libro bean = getLibro(form);
-			Libro beanAct = em.find(Libro.class, id);
-			beanAct.setArgumento(bean.getArgumento());
-			beanAct.setAutor(bean.getAutor());
-			beanAct.setNombre(bean.getNombre());
-			beanAct.setPaginas(bean.getPaginas());
+			Libro bean = mapper.map(form, Libro.class);
+			// Libro beanAct = em.find(Libro.class, id);
+			// beanAct.setArgumento(bean.getArgumento());
+			// beanAct.setAutor(bean.getAutor());
+			// beanAct.setNombre(bean.getNombre());
+			// beanAct.setPaginas(bean.getPaginas());
 			em.merge(bean);
 			transaction.commit();
 		} catch (Exception except) {
@@ -163,11 +133,11 @@ public class LibroRest {
 		logger.info("LibroRest - remove - init");
 		EntityManager em = EMF.get().createEntityManager();
 		EntityTransaction transaction = em.getTransaction();
-		logger.info("Transaction actived ? - "+transaction.isActive());
+		logger.info("Transaction actived ? - " + transaction.isActive());
 		transaction.begin();
-		logger.info("Transaction actived ? - "+transaction.isActive());
+		logger.info("Transaction actived ? - " + transaction.isActive());
 		try {
-			logger.info("Vamos a borrar a: "+id);
+			logger.info("Vamos a borrar a: " + id);
 			Libro bean = em.find(Libro.class, id);
 			em.remove(bean);
 			transaction.commit();
